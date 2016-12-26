a90669536c53f3d9e2e2454a14825c073b258139
Alexander Vinober <aliaksandr_vinober@epam.com>
2016-12-26 13:52:32 +0300

    more than one call test update

--- katas/more-than-one-call/more-than-one-call_test.js
+++ katas/more-than-one-call/more-than-one-call_test.js
@@ -1,8 +1,8 @@
-describe('sum function', function() {
+describe('more than one call', function() {
 
     it('should return the sum of the numbers.', function() {
-        expect(sum(2,3)).toBe(5);
-        expect(sum(2)(3)).toBe(5);
-        expect(sum(3)(3)(1)).toBe(7);
+        expect(sum(2,3) == 5).toBeTruthy();
+        expect(sum(2)(3) == 5).toBeTruthy();
+        expect(sum(3)(3)(1) == 7).toBeTruthy();
     });
 });
